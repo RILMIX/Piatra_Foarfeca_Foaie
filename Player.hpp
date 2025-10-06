5abe52bc1fef15e5eb871dd15fcf38a550ef036d
RILMIX <relisam3@gmail.com>
2025-10-06 21:55:21 +0300

    Player.cpp added for lab02

--- Player.hpp
+++ Player.hpp
@@ -2,32 +2,14 @@
 
 #include <iostream>
 #include <limits>
-#include "choice.hpp"
+#include "Choice.hpp"
 
 class Player {
 private:
-    Choice selectedChoice;
+    Choice selected;
 
 public:
-    void chooseOption() {
-        int value = -1;
-        bool valid = false;
-
-        while (!valid) {
-            std::cout << "Alege o opțiune: 0 = Rock, 1 = Paper, 2 = Scissors -> ";
-            if (std::cin >> value && value >= 0 && value <= 2) {
-                selectedChoice = static_cast<Choice>(value);
-                valid = true;
-            }
-            else {
-                std::cout << "Input invalid. Introdu un număr între 0 și 2.\n";
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
-    }
-
-    Choice getSelectedChoice() const {
-        return selectedChoice;
-    }
+    void choose();
+    Choice getSelected() const;
+    void showChoice() const;
 };
