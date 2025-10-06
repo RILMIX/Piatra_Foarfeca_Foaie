cc61378d8d61d0f37027ab0a421fa458663af785
RILMIX <relisam3@gmail.com>
2025-10-06 21:58:10 +0300

    Build.bat added for lab02

---
+++ Build.bat
@@ -0,0 +1,31 @@
+﻿@echo off
+echo === Compilare joc Piatră-Hârtie-Foarfecă ===
+
+:: Compilăm fișierele sursă
+g++ -c player.cpp -o player.o
+if errorlevel 1 goto fail
+
+g++ -c computer.cpp -o computer.o
+if errorlevel 1 goto fail
+
+g++ -c engine.cpp -o engine.o
+if errorlevel 1 goto fail
+
+g++ -c main.cpp -o main.o
+if errorlevel 1 goto fail
+
+:: Legăm toate obiectele într-un executabil
+g++ player.o computer.o engine.o main.o -o game.exe
+if errorlevel 1 goto fail
+
+echo -----------------------------
+echo Compilare reușită!
+echo Rulează „game.exe” pentru a începe jocul.
+goto end
+
+:fail
+echo Eroare la compilare!
+echo Verifică fișierele sursă și încearcă din nou.
+
+:end
+exit /b
